6f69fcb466179f453d2b190396c0d991f9d95a54
hoangnguyen02 <thhoang.nguyen02@gmail.com>
2025-10-31 11:57:13 +0700

    test CI trigger on feature branch

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const router = Router()
 
 router.get('/', (req, res) => {
     const responseJson = {
-        message: 'Welcome warriors to Golden Owl!',
+        message: 'Welcome warriors to Golden Owl! (feature branch test)',
     }
     res.json(responseJson)
 })
